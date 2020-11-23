5a98b560e09a93c2699133b5b9a8ed55a78f99ce
lxhom <lxhomeis@gmail.com>
2020-11-23 21:23:48 +0100

    trying the cpp class thingy instead of the whole array mess (doesnt work rn tho cuz me is a bit dumb)

--- widget.h
+++ widget.h
@@ -12,6 +12,7 @@ class Widget : public QWidget {
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+    Squid test;
 };
 
 class Squid {
@@ -19,11 +20,12 @@ private:
     // the movement, pos and so on
     // [2] = x, y | [3] = r, g, b
     int speed[2];
+    int windowSize[2];
+    int squidSize;
     int direction[2];
     int position[2];
     int color[3];
-    int size;
-    int borders[2];
+
 public:
     // constructor, deconstructor
     Squid(int width, int height);
@@ -34,6 +36,26 @@ public:
 };
 
 Squid::Squid(int width, int height) {
+
+    this->speed[0] = rand()%10+1;
+    this->speed[1] = rand()%10+1;
+
+    this->windowSize[0] = width;
+    this->windowSize[1] = height;
+
+    this->squidSize = rand()%15+5;
+
+    // r%2 : 0 / 1 | r%2*2 : 0 / 2 | r%2*2-1 : -1 / 1 
+    this->direction[0] = rand()%2*2-1;
+    this->direction[1] = rand()%2*2-1;
+
+    this->position[0] = rand()%(this->windowSize[0] - this->squidSize);
+    this->position[1] = rand()%(this->windowSize[0] - this->squidSize);
+
+    this->color[0] = rand()%200+25;
+    this->color[1] = rand()%200+25;
+    this->color[2] = rand()%200+25;
+
 
 }
 
